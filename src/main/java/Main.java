aeb179c0e2fedc8d86f672ba6e8eb81c5d878979
Oscar 8A <oscarochoacerrato@gmail.com>
2022-06-22 15:24:56 -0400

    implement logic with while & for loops

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,32 @@
 public class Main {
+    // public static void main(String[] args) {
+    //     // simulate the passing of time
+    //     int startingYear = 2000;
+    //     int targetYear = 2011;
+    //     int currentYear = startingYear + 1;
+    //     while (currentYear < targetYear) {
+    //         // conditional logic based on the current year
+    //         if (currentYear - startingYear == 1) {
+    //             System.out.println((currentYear - startingYear) + " year has passed");
+    //         } else {
+    //             System.out.println((currentYear - startingYear) + " years have passed");
+    //         }
+    //         currentYear++;
+    //     }
+    // }
+
     public static void main(String[] args) {
         // simulate the passing of time
         int startingYear = 2000;
         int targetYear = 2011;
-        int currentYear = startingYear;
-        while (currentYear < targetYear) {
-            System.out.println((currentYear - startingYear) + " year(s) have passed");
+
+        for (int currentYear = 2001; currentYear < targetYear; currentYear++) {
             // conditional logic based on the current year
-            currentYear++;
+            if (currentYear - startingYear == 1) {
+                System.out.println((currentYear - startingYear) + " year has passed");
+            } else {
+                System.out.println((currentYear - startingYear) + " years have passed");
+            }
         }
     }
 }
